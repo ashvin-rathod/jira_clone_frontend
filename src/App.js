f04b0875748e23774daece14992280e15df637f8
yagnik viradiya <yagnik.vhits@gmail.com>
2021-11-09 16:09:23 +0530

    fatal: repository 'https://github.com/ashvin-rathod/jira_clone_backend.git/' not found

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div >
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      hello
     </div>
   );
 }
